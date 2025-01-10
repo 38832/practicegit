3728c7aa1742bebb8952aa961ce5c0df1a1c406b
Abutalha Shaikh <abutalha11503@gmail.com>
2025-01-10 17:34:49 +0530

    my second change

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-def addition(a,b,):
+def addition(a,b):
     return a+b
 
-def substraction(a, b, e):
-    return a - b - e
+def substraction(a, b, d):
+    return a - b - d
 
 def division(a, b):
     return a / b
+
+def multiplication(a, b):
+    return a * b
