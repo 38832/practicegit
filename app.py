6a3bae0042217bc8c38017de142a1b86081c1b1b
Abutalha Shaikh <117577735+38832@users.noreply.github.com>
2025-01-10 17:40:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-def addition(a,b):
-    return a+b
+def addition(e,f):
+    return e+f
 
-def substraction(a, b, e):
-    return a - b - e
+def substraction(g, h, i):
+    return g - h - i
 
 def division(a, b):
     return a / b
